41473985824357b408b303e4464455ae7cd82b70
mahi-vea <shakthimagenderan@gmail.com>
2025-10-19 21:18:30 +0530

    Task 2

--- swift_pico/launch/task_2b.launch.py
+++ swift_pico/launch/task_2b.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
         launch_arguments={'gz_args': PathJoinSubstitution([
             pkg_project_gazebo,
             'worlds',
-            'swift_pico.sdf -r'
+            'swift_pico_2b.sdf -r'
         ])}.items(),
     )
     
