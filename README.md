0f4fbddecb21329f64b9c245315886384ee32e93
Himanshu Umap <158447620+Himanshu-Umap@users.noreply.github.com>
2025-10-24 16:45:15 +0530

    merged task1c(drone's controller) with main for task2(#3)
    * Added .gitignore file and started task1c
    
    * Completed pico_controller_PID_cpp script, error in building pid_msg/msg/pid_tune.hpp not found
    
    * Changed pid_tune to controller_msg everywhere
    
    * cpp stable v1
    
    * hardcoded tuned PID with error margin of +-0.5
    
    * Updated the readme for the task1c execution command
    
    * Improved formatting in readme
    
    * submitted pid code of 38 marks in pico_controller_PID.cpp
    
    * Corrected finally submitted 39 marks and 38 marks code
    
    * stable v2: for working on task2

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # eYRC25-26 Krishi Drone Theme
 
+## Task 1c guide:
+mkdir ~/pico_ws<br>
+cd ~/pico_ws/<br>
+using ssh: **git clone -b task1/task1c git@github.com:Himanshu-Umap/krishi-drone.git --recursive src**<br>
+or <br>
+using https: **git clone -b task1/task1c https://github.com/Himanshu-Umap/krishi-drone.git  --recursive src**<br>
+colcon build<br>
+
+every time you build or start new terminal run: **source ~/.bashrc**<br>
+open a new terminal and run: **ros2 launch swift_pico task_1c.launch.py**<br>
+In new treminal run: **ros2 run swift_pico pico_controller_PID_cpp**<br>
+For recording: press **ctrl+alt+shift+R**<br>
+
+To launch Plotjuggler for visualizing ROS 2 topics, run: **ros2 run plotjuggler plotjuggler**<br>
+
+
 ## Problem Statement:
 The goal of this challenge is to navigate a nano drone through a smart greenhouse, guided by real-time vision from an overhead camera, detect infested plants and performs precise, targeted pesticide pickup and application to affected crop areas.
 
--- swift_pico/src/pico_controller_PID.cpp
+++ swift_pico/src/pico_controller_PID.cpp
@@ -1,3 +1,4 @@
+// FINAL SUBMITTED 38 MARKS CODE
 /*This cpp file runs a ROS 2-node of name pico_control which holds the position of Swift Pico Drone on the given dummy.
 This node publishes and subsribes the following topics:
 
@@ -14,11 +15,13 @@ CODE MODULARITY AND TECHNIQUES MENTIONED LIKE THIS WILL HELP YOU GAINING MORE MA
 #include <chrono>
 #include <functional>
 #include <memory>
+#include <vector>
+#include <algorithm>
 
 #include "geometry_msgs/msg/pose_array.hpp"
 #include "swift_msgs/msg/swift_msgs.hpp"
-#include "pid_msg/msg/pid_tune.hpp"
-#include "pid_msg/msg/pid_error.hpp"
+#include "controller_msg/msg/pid_tune.hpp"
+#include "error_msg/msg/error.hpp"  
 #include "rclcpp/rclcpp.hpp"
 
 using std::placeholders::_1;
@@ -56,8 +59,8 @@ class Swift_Pico : public rclcpp::Node
 
             /* [x_setpoint, y_setpoint, z_setpoint]
             whycon marker at the position of the dummy given in the scene. Make the whycon marker associated with position_to_hold dummy renderable and make changes accordingly*/
-            setpoint[0] = 2;
-            setpoint[1] = 2;
+            setpoint[0] = -7;
+            setpoint[1] = 0;
             setpoint[2] = 20;
 
             //Declaring a cmd of message type swift_msgs and initializing values
@@ -72,25 +75,30 @@ class Swift_Pico : public rclcpp::Node
             //proportional
             Kp[0] = 0;
             Kp[1] = 0;
-            Kp[2] = 0;
+            // Kp[2] = 0;
+            Kp[2] = 60.5;
+            // Kp[2] = 14.1;
 
             //integral
             Ki[0] = 0;
             Ki[1] = 0;
-            Ki[2] = 0;
+            // Ki[2] = 0;
+            Ki[2] = 0.108;
+            // Ki[2] = 0.104;
 
             //derivative
             Kd[0] = 0;
             Kd[1] = 0;
-            Kd[2] = 0;
+            // Kd[2] = 0;
+            Kd[2] = 1600;
+            // Kd[2] = 282;
             /*-----------------------Add other required variables for pid here ----------------------------------------------*/
 
+            prev_error[0] = 0.0f; prev_error[1] = 0.0f; prev_error[2] = 0.0f; // [roll, pitch, throttle]
+            error_sum[0] = 0.0f; error_sum[1] = 0.0f; error_sum[2] = 0.0f;    //  [roll, pitch, throttle]
 
-
-
-
-
-
+            max_values[0] = 2000; max_values[1] = 2000; max_values[2] = 5000;
+            min_values[0] = 1000; min_values[1] = 1000; min_values[2] = 1000;
 
 
             /* Hint : Add variables for storing previous errors in each axis, like prev_error = [0,0,0] where corresponds to [pitch, roll, throttle]. 
@@ -102,29 +110,28 @@ class Swift_Pico : public rclcpp::Node
 
             /*This is the sample time in which you need to run pid. Choose any time which you seem fit.*/
         
-            sample_time = 60ms; //in milli-seconds
+            sample_time = 40ms; //in milli-seconds
 
             //Publishing /drone_command, /throttle_error, /pitch_error, /roll_error
             command_pub = this->create_publisher<swift_msgs::msg::SwiftMsgs>("/drone_command", 10);
-            pid_error_pub = this->create_publisher<pid_msg::msg::PIDError>("/pid_error", 10);
+            pid_error_pub = this->create_publisher<error_msg::msg::Error>("/pid_error", 10);
 
 
-            //Add othher ROS 2 Publishers here
+            //Add other ROS 2 Publishers here
 
             //Subscribing to /whycon/poses, /throttle_pid, /pitch_pid, roll_pid
             whycon_sub = this->create_subscription<geometry_msgs::msg::PoseArray>("/whycon/poses", 1, std::bind(&Swift_Pico::whycon_callback, this, _1));
-            throttle_pid_sub = this->create_subscription<pid_msg::msg::PIDTune>("/throttle_pid", 1, std::bind(&Swift_Pico::altitude_set_pid, this, _1));
+            throttle_pid_sub = this->create_subscription<controller_msg::msg::PIDTune>("/throttle_pid", 1, std::bind(&Swift_Pico::altitude_set_pid, this, _1));
 
             //------------------------Add other ROS 2 Subscribers here-----------------------------------------------------
-
+            roll_pid_sub = this->create_subscription<controller_msg::msg::PIDTune>("/roll_pid", 1, std::bind(&Swift_Pico::roll_set_pid, this, _1));
+            pitch_pid_sub = this->create_subscription<controller_msg::msg::PIDTune>("/pitch_pid", 1, std::bind(&Swift_Pico::pitch_set_pid, this, _1));
 
             //Arming the drone
             arm();
 
             //Creating a timer to run the pid function periodically, refer ROS 2 tutorials on how to create a publisher subscriber(C++)
-
-
-
+            pid_timer_ = this->create_wall_timer(sample_time, std::bind(&Swift_Pico::pid, this));
 
         }
 
@@ -133,18 +140,37 @@ class Swift_Pico : public rclcpp::Node
         //declare all the variables, arrays, strcuts etc. here
         float drone_position[3];
         int setpoint[3];
-        CMD cmd;
+        swift_msgs::msg::SwiftMsgs cmd;
         std::chrono::milliseconds sample_time;
         float Kp[3];
         float Ki[3];
         float Kd[3];
+        
+        //variables for PID control
+        float prev_error[3]; 
+        float error_sum[3];
+        float max_values[3];    // max values for [roll, pitch, throttle]
+        float min_values[3];    // min values for [roll, pitch, throttle]
+
+        float out_roll;
+        float out_pitch;
+        float out_throttle;
+
+        // NEW VARIABLES FOR D-TERM FILTER
+        float d_filter_alpha = 0.6f; // Filter coefficient (0=max filter, 1=no filter)
+        float filtered_derivative[3] = {0.0f, 0.0f, 0.0f};
 
         //declare the publishers and subscribers here
         rclcpp::Publisher<swift_msgs::msg::SwiftMsgs>::SharedPtr command_pub;
-        rclcpp::Publisher<pid_msg::msg::PIDError>::SharedPtr pid_error_pub;
+        rclcpp::Publisher<error_msg::msg::Error>::SharedPtr pid_error_pub;
 
         rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr whycon_sub;
-        rclcpp::Subscription<pid_msg::msg::PIDTune>::SharedPtr throttle_pid_sub;
+        rclcpp::Subscription<controller_msg::msg::PIDTune>::SharedPtr throttle_pid_sub;
+        rclcpp::Subscription<controller_msg::msg::PIDTune>::SharedPtr pitch_pid_sub;
+        rclcpp::Subscription<controller_msg::msg::PIDTune>::SharedPtr roll_pid_sub;
+
+        // ROS2 Timer
+        rclcpp::TimerBase::SharedPtr pid_timer_;
 
         //define functions and callbacks here
 
@@ -169,41 +195,60 @@ class Swift_Pico : public rclcpp::Node
             cmd.rc_throttle = 1500;
             cmd.rc_aux4 = 2000;
             command_pub->publish(cmd);
-
         }
 
         void whycon_callback(const geometry_msgs::msg::PoseArray & msg)
         {
+            // the first pose in the array is the drone's pose.
+            if (msg.poses.empty()) return; // Safety check for empty message
+
             drone_position[0] = msg.poses[0].position.x;
-            /*--------------------Set the remaining co-ordinates of the drone from msg----------------------------------------------
-
-
-
+            
+            // my code
+            //--------------------Set the remaining co-ordinates of the drone from msg----------------------------------------------
+            drone_position[1] = msg.poses[0].position.y;
+            drone_position[2] = msg.poses[0].position.z;
 	
-		    ------------------------------------------------------------------------------------------------------------------------*/
+		    //------------------------------------------------------------------------------------------------------------------------
+        }
 
+        //----------------------------Define callback function like altitide_set_pid to tune pitch, roll--------------
+        //[roll, pitch, throttle]
+        
+        //Callback function for /roll_pid
+        void roll_set_pid(const controller_msg::msg::PIDTune & roll)
+        {
+            Kp[0] = roll.kp * 0.03;  // This is just for an example. You can change the ratio/fraction value accordingly
+		    Ki[0] = roll.ki * 0.008;
+		    Kd[0] = roll.kd * 0.6;
+        }
+
+        //Callback function for /pitch_pid
+        void pitch_set_pid(const controller_msg::msg::PIDTune & pitch)
+        {
+            Kp[1] = pitch.kp * 0.03;  // This is just for an example. You can change the ratio/fraction value accordingly
+		    Ki[1] = pitch.ki * 0.008;
+		    Kd[1] = pitch.kd * 0.6;
         }
 
         //Callback function for /throttle_pid
 	    //This function gets executed each time when /drone_pid_tuner publishes /throttle_pid
 	
-        void altitude_set_pid(const pid_msg::msg::PIDTune & alt)
+        void altitude_set_pid(const controller_msg::msg::PIDTune & alt)
         {
-            Kp[2] = alt.kp * 0.03;  // This is just for an example. You can change the ratio/fraction value accordingly
-		    Ki[2] = alt.ki * 0.008;
-		    Kd[2] = alt.kd * 0.6;
+            // Kp[2] = alt.kp * 0.03;  // This is just for an example. You can change the ratio/fraction value accordingly
+		    // Ki[2] = alt.ki * 0.008;
+		    // Kd[2] = alt.kd * 0.6;
+            (void)alt;
         }
-
-
-        //----------------------------Define callback function like altitide_set_pid to tune pitch, roll--------------
 
         //----------------------------------------------------------------------------------------------------------------------
 
 
         void pid()
         {
-            auto cmd = swift_msgs::msg::SwiftMsgs();
-            auto error_pub = pid_msg::msg::PIDError();
+                
+            auto error_pub = error_msg::msg::Error();
             /*-----------------------------Write the PID algorithm here--------------------------------------------------------------
 
             # Steps:
@@ -216,26 +261,82 @@ class Swift_Pico : public rclcpp::Node
             #																														self.cmd.rcPitch = self.max_values[1]
             #	7. Update previous errors.eg: self.prev_error[1] = error[1] where index 1 corresponds to that of pitch (eg)
             #	8. Add error_sum
+            
+            */
 
+            // Step 1: Compute error. Note the axis mapping: Pitch corrects X, Roll corrects Y.
+            // Index mapping: [0] = Roll, [1] = Pitch, [2] = Throttle
+            float error[3];
+            error[0] =  setpoint[0] - drone_position[0];
+            error[1] = drone_position[1] - setpoint[1];
+            error[2] = setpoint[2] - drone_position[2];
 
+            // Step 2 & 3: Compute P, I, D terms and total output
+            // Proportional
+            float p_out[3];
+            p_out[0] = Kp[0] * error[0];
+            p_out[1] = Kp[1] * error[1];
+            p_out[2] = Kp[2] * error[2];
 
+            // Integral with anti-windup
+            error_sum[0] += error[0];
+            error_sum[1] += error[1];
+            error_sum[2] += error[2];
+            error_sum[0] = ((error_sum[0] < -400.0f) ? -400.0f : (error_sum[0] > 400.0f) ? 400.0f : error_sum[0]);
+            error_sum[1] = ((error_sum[1] < -400.0f) ? -400.0f : (error_sum[1] > 400.0f) ? 400.0f : error_sum[1]);
+            error_sum[2] = ((error_sum[2] < -400.0f) ? -400.0f : (error_sum[2] > 400.0f) ? 400.0f : error_sum[2]);
 
+            float i_out[3];
+            i_out[0] = Ki[0] * error_sum[0];
+            i_out[1] = Ki[1] * error_sum[1];
+            i_out[2] = Ki[2] * error_sum[2];
 
+            // Derivative
+            // float d_out[3];
+            // d_out[0] = Kd[0] * (error[0] - prev_error[0]);
+            // d_out[1] = Kd[1] * (error[1] - prev_error[1]);
+            // d_out[2] = Kd[2] * (error[2] - prev_error[2]);
 
+            // Derivative with Low-Pass Filter
+            float d_out[3];
+            float raw_derivative[3];
 
+            // Calculate raw (noisy) derivative for all axes
+            raw_derivative[0] = error[0] - prev_error[0];
+            raw_derivative[1] = error[1] - prev_error[1];
+            raw_derivative[2] = error[2] - prev_error[2];
 
+            // Apply the exponential moving average filter
+            for (int i = 0; i < 3; ++i) {
+                filtered_derivative[i] = (d_filter_alpha * filtered_derivative[i]) + ((1 - d_filter_alpha) * raw_derivative[i]);
+                d_out[i] = Kd[i] * filtered_derivative[i];
+            }
 
+            // Total PID output
+            float out_roll = p_out[0] + i_out[0] + d_out[0];
+            float out_pitch = p_out[1] + i_out[1] + d_out[1];
+            float out_throttle = p_out[2] + i_out[2] + d_out[2];
 
+            // Step 4: Apply output to the neutral command value (1500)
+            this->cmd.rc_roll = 1500 + static_cast<int>(out_roll);
+            this->cmd.rc_pitch = 1500 + static_cast<int>(out_pitch);
+            this->cmd.rc_throttle = 1500 - static_cast<int>(out_throttle);
 
+            // Step 6: Limit the final command values
+            this->cmd.rc_roll = (this->cmd.rc_roll < min_values[0]) ? min_values[0] : ((this->cmd.rc_roll > max_values[0]) ? max_values[0] : this->cmd.rc_roll);
+            this->cmd.rc_pitch = (this->cmd.rc_pitch < min_values[1]) ? min_values[1] : ((this->cmd.rc_pitch > max_values[1]) ? max_values[1] : this->cmd.rc_pitch);
+            this->cmd.rc_throttle = (this->cmd.rc_throttle < min_values[2]) ? min_values[2] : ((this->cmd.rc_throttle > max_values[2]) ? max_values[2] : this->cmd.rc_throttle);
 
+            // Update the previous error
+            prev_error[0] = error[0];
+            prev_error[1] = error[1];
+            prev_error[2] = error[2];
 
-
-
-
-
-
-            #------------------------------------------------------------------------------------------------------------------------*/
-            command_pub->publish(cmd);
+            // #------------------------------------------------------------------------------------------------------------------------
+            command_pub->publish(this->cmd);
+            error_pub.roll_error = error[0];
+            error_pub.pitch_error = error[1];
+            error_pub.throttle_error = error[2];
             //calculate throttle error, pitch error and roll error, then publish it accordingly
             pid_error_pub->publish(error_pub);
 
